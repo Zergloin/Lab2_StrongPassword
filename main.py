da4180b6e6dca47c2212629134211a1e206cad21
Zergloin <alexsey.hod@mail.ru>
2024-11-27 20:05:16 +0300

    feat: add sample functionality to main

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+from InputFromUser import check_user_input
+from SearchFromFile import find_password_file
+from SearchFromSite import find_password_site
+
 if __name__ == '__main__':
-    pass
+    check_user_input()
+    find_password_file(input('Enter the path to the file: '))
+    find_password_site(input('Enter the URL: '))
